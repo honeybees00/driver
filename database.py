1bf022c87b99b74f09c170ecf0d0ea019cae9358
Christina Ortiz <ortizchristina881@gamil.com>
2026-05-04 20:18:46 -0400

    Table corrections

--- database.py
+++ database.py
@@ -19,33 +19,34 @@ def init_db():
     conn = get_connection()
     cur = conn.cursor()
     cur.execute("""
- create table if not exist driver(
+ create table if not exists driver(
     driver_id serial Primary Key,
     name varchar (50) not null,
    license_type varchar (50) not null
-                             )
- create table if not exist route(
+                             );
+ create table if not exists route(
    route_id serial Primary key,
    service_zone Varchar (50) not null,
     date date
+                );
+create table if not exists driver(
     driver_id int ,
     foreign key (driver_id) references driver(driver_id)                            
-                                                  
-                 ) 
- create table if not exist vehicle(
+                                                  ); 
+ create table if not exists vehicle(
     vehicle_id serial Primary key,
       license_plate varchar (50) not null,
        model varchar (50) not nul,
        driver_id int unique,
           foreign key (driver_id) refernces driver(driver_id)                                       
-      )
-create table if not exist package(
+      );
+create table if not exists package(
    package_id serial Primary Key,
     description varchar (75) not null,
     weight int (50) not null,
     route_id int  
      foreign key (route_id) refernces route(route_id)                        
-                            ) 
+                            );
 
                                                                                                                                        )                                                     
 
